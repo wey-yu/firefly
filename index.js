594ad56ee27c4b64078dcb2cd0d0c904bcaca545
Laurent Doguin <laurent.doguin@gmail.com>
2020-03-20 14:16:42 +0100

    app dynamics

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const apm = require('elastic-apm-node').start();
+const appdy = require("appdynamics").profile({
+ controllerHostName: process.env.APPDYNAMICS_CONTROLLER_HOST_NAME,
+ controllerPort: process.env.APPDYNAMICS_CONTROLLER_PORT,
+ controllerSslEnabled: process.env.APPDYNAMICS_CONTROLLER_SSL_ENABLED,
+ accountName: process.env.APPDYNAMICS_AGENT_ACCOUNT_NAME,
+ accountAccessKey: process.env.APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY,
+ applicationName: 'firefly',
+ tierName: process.env.APP_ID,
+ nodeName: process.env.INSTANCE_ID // The controller will automatically append the node name with a unique number
+});
+
 const ServicesChecker = require('firefly-server').ServicesChecker
 
 //const Db = require('firefly-core-libs').MemDb
